24bed9221f34f1865ad181eb5d86c1150c3474d1
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-03-30 22:11:09 +0600

    Modify login page

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -1,30 +1,44 @@
-import { FormEventHandler } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
+import { FormEventHandler } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
 // import PrimaryButton from '@/Components/PrimaryButton';
 // import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
-import { Input } from '@/Components/ui/input';
-import { Button } from '@/Components/ui/button';
+import { Head, Link, useForm } from "@inertiajs/react";
+import { Input } from "@/Components/ui/input";
+import { Button } from "@/Components/ui/button";
+import { Card, CardContent } from "@/Components/ui/card";
+import {
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldSeparator,
+} from "@/Components/ui/field";
+import { Field } from "@headlessui/react";
 
-export default function Login({ status, canResetPassword }: { status?: string, canResetPassword: boolean }) {
+export default function Login({
+    status,
+    canResetPassword,
+}: {
+    status?: string;
+    canResetPassword: boolean;
+}) {
     const { data, setData, post, processing, errors, reset } = useForm<{
         email: string;
         password: string;
         remember: boolean;
     }>({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
 
-        post(route('login'), {
-            onFinish: () => reset('password'),
+        post(route("login"), {
+            onFinish: () => reset("password"),
         });
     };
 
@@ -32,62 +46,148 @@ export default function Login({ status, canResetPassword }: { status?: string, c
         <GuestLayout>
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
 
             <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-                    <Input
-                       id='email'
-                       type='email'
-                       value={data.email}
-                       className='mt-1 block w-full shadow-lg'
-                       autoComplete='username'
-                       onChange={ (e) => setData('email', e.target.value)}
-                    />
+                <div className="flex flex-col gap-6">
+                    <Card className="overflow-hidden p-0">
+                        <CardContent className="p-0">
+                            <div className="p-6 md:p-8">
+                                <FieldGroup>
+                                    <div className="flex flex-col items-center gap-2 text-center">
+                                        <h1 className="text-2xl font-bold">
+                                            Welcome back
+                                        </h1>
+                                    </div>
+                                    <Field>
+                                        <FieldLabel htmlFor="email">
+                                            Email
+                                        </FieldLabel>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            value={data.email}
+                                            className="mt-1 block w-full shadow-lg"
+                                            autoComplete="username"
+                                            onChange={(e) =>
+                                                setData("email", e.target.value)
+                                            }
+                                        />
+                                        <InputError
+                                            message={errors.email}
+                                            className="mt-2"
+                                        />
+                                    </Field>
+                                    <Field>
+                                        <div className="flex items-center">
+                                            <FieldLabel htmlFor="password">
+                                                Password
+                                            </FieldLabel>
+                                            {canResetPassword && (
+                                                <a
+                                                    href={route(
+                                                        "password.request",
+                                                    )}
+                                                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                                                >
+                                                    Forgot your password?
+                                                </a>
+                                            )}
+                                        </div>
+                                        <Input
+                                            id="password"
+                                            type="password"
+                                            value={data.password}
+                                            className="mt-1 block w-full shadow-lg"
+                                            autoComplete="current-password"
+                                            onChange={(e) =>
+                                                setData(
+                                                    "password",
+                                                    e.target.value,
+                                                )
+                                            }
+                                        />
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-                     <Input
-                       id='password'
-                       type='password'
-                       value={data.password}
-                       className='mt-1 block w-full shadow-lg'
-                       autoComplete='current-password'
-                       onChange={ (e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <Button className='ms-4' disabled={processing}>
-                        Log In
-                    </Button>
-
+                                        <InputError
+                                            message={errors.password}
+                                            className="mt-2"
+                                        />
+                                    </Field>
+                                    <Field>
+                                        <div className="block mt-0">
+                                            <label className="flex items-center">
+                                                <Checkbox
+                                                    name="remember"
+                                                    checked={data.remember}
+                                                    onChange={(e) =>
+                                                        setData(
+                                                            "remember",
+                                                            e.target.checked,
+                                                        )
+                                                    }
+                                                />
+                                                <span className="ms-2 text-sm text-gray-600">
+                                                    Remember me
+                                                </span>
+                                            </label>
+                                        </div>
+                                    </Field>
+                                    <Field>
+                                        <Button
+                                            className="ms-2"
+                                            disabled={processing}
+                                        >
+                                            Log In
+                                        </Button>
+                                    </Field>
+                                    <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+                                        Or continue with
+                                    </FieldSeparator>
+                                    <Field className="grid gap-4 sm:grid-cols-2">
+                                        <Button variant="outline" type="button">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                className="w-5 h-5 gap-2 me-2"
+                                            >
+                                                <path
+                                                    d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                                                    fill="currentColor"
+                                                />
+                                            </svg>
+                                            Continue with Apple
+                                        </Button>
+                                        <Button variant="outline" type="button">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                className="w-5 h-5 gap-2 me-2"
+                                            >
+                                                <path
+                                                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                                    fill="currentColor"
+                                                />
+                                            </svg>
+                                            Continue with Google
+                                        </Button>
+                                    </Field>
+                                    <FieldDescription className="text-center">
+                                        Don&apos;t have an account?{" "}
+                                        <a href="#">Sign up</a>
+                                    </FieldDescription>
+                                </FieldGroup>
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <FieldDescription className="px-6 text-center">
+                        By clicking continue, you agree to our{" "}
+                        <a href="#">Terms of Service</a> and{" "}
+                        <a href="#">Privacy Policy</a>.
+                    </FieldDescription>
                 </div>
             </form>
         </GuestLayout>
